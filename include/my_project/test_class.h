aaaff7f44664f2eac67b6ef8590010c20e983360
Paxton Terry <PaxtonTerryDev@gmail.com>
2025-05-14 14:59:14 -0600

    Add Test_Class and integrate quant-config library
    Introduced `Test_Class` with a simple `Howdy` method and updated `main.cpp` to use it. Integrated `quant-config` library for configuration management and adjusted CMake setup to include necessary files and dependencies.

---
+++ include/my_project/test_class.h
@@ -0,0 +1,17 @@
+//
+// Created by grayhaven on 5/14/25.
+//
+
+#ifndef TEST_CLASS_H
+#define TEST_CLASS_H
+#include <iostream>
+
+
+class Test_Class {
+public:
+    static void Howdy();
+};
+
+
+
+#endif //TEST_CLASS_H
---
+++ src/my_project/test_class.cpp
@@ -0,0 +1,10 @@
+//
+// Created by grayhaven on 5/14/25.
+//
+
+#include "my_project/test_class.h"
+
+void Test_Class::Howdy() {
+    std::cout << "Hello World!" << std::endl;
+}
+
